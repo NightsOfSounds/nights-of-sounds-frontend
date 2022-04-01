f82be083d4aca6eb2b0ff2c809284f3e37c7f028
Akkjon <akkjon@outlook.de>
2022-04-01 05:00:54 +0200

    Mobile layout and language improvements

--- src/About.tsx
+++ src/About.tsx
@@ -1,7 +1,7 @@
 import { ContentBox, HoverImg, ImageTextBoxType, SiteTitle, TextProcessor } from "./App"
 import Profile01 from './img/profile01.svg'
 import Profile02 from './img/profile02.svg'
-import { Box, Paper, Stack, Typography } from "@mui/material"
+import { Box, Paper, Stack, Typography, useMediaQuery, useTheme } from "@mui/material"
 import { useLanguage } from "./Localization"
 
 function About() {
@@ -32,11 +32,20 @@ interface ImageHorizontalBoxType extends ImageTextBoxType {
 }
 function ImageHorizontalBox({name, text, imgUrl, alignment}:ImageHorizontalBoxType) {
 
-  const el1 = <Box sx={{overflow: "hidden", width: "50%", display: "flex"}}>
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"))
+
+  const el1 = <Box sx={{
+                  overflow: "hidden", 
+                  width: {xs: 300, md: 1/2}, 
+                  maxWidth: "100%", 
+                  display: "inline",
+                  margin: isMobile ? "auto" : ""
+                  }}>
                 <HoverImg src={imgUrl} alt="User"/>
               </Box>
 
-  const el2 = <Box sx={{width: "50%", textAlign: (alignment === 0 ? "left" : "right")}}>
+  const el2 = <Box sx={{width: {xs: 1, md:  1/2}, textAlign: (isMobile ? "center" : (alignment === 0 ? "left" : "right"))}}>
                 <Typography variant="h4">{name}</Typography>
                 <TextProcessor>{text}</TextProcessor>
               </Box>
@@ -45,7 +54,7 @@ function ImageHorizontalBox({name, text, imgUrl, alignment}:ImageHorizontalBoxTy
     width: "100%",
     padding: "20px",
   }}>
-    <Stack direction="row" sx={{width: "100%"}} gap={2}>
+    <Stack direction={{xs: "column", md: "row"}} sx={{width: "100%"}} gap={2}>
       {alignment === 0 ? el1 : el2}
       {alignment === 0 ? el2 : el1}
       
