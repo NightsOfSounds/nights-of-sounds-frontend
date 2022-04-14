f4e07e17e75ef2ddd5738d0574a3a75f6af53427
Akkjon <akkjon@outlook.de>
2022-04-14 23:25:31 +0200

    fixes embed not loading on language change

--- src/components/social/YoutubeEmbed.tsx
+++ src/components/social/YoutubeEmbed.tsx
@@ -1,5 +1,5 @@
 import { styled } from "@mui/material";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type YoutubeEmbedType = {
     index?: number,
@@ -12,8 +12,10 @@ function YoutubEmbed({index = 0, controls = false, autoplay = false}:YoutubeEmbe
 
     const ytChannelId = "UCn1QDZMJicrd3CutKTr5Hhg"
     const reqURL = "https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent("https://www.youtube.com/feeds/videos.xml?channel_id=");
+    const [url, setUrl] = useState<string|undefined>()
 
     useEffect(() => {
+        if(url) return
         if(!ref) return
         if((typeof Worker) === "undefined") return
         const worker = new Worker("./workers/youtubeWorker.js")
@@ -26,9 +28,9 @@ function YoutubEmbed({index = 0, controls = false, autoplay = false}:YoutubeEmbe
         )
         worker.onmessage = (event: MessageEvent) => {
             const { data } = event
-            ref.current?.setAttribute("src", `https://youtube.com/embed/${data}?controls=${controls ? 1 : 0}&autoplay=${autoplay ? 1 : 0}`)
+            setUrl(`https://youtube.com/embed/${data}?controls=${controls ? 1 : 0}&autoplay=${autoplay ? 1 : 0}`)
         }
-    }, [ref, reqURL, ytChannelId, index, controls, autoplay])
+    }, [url, ref, reqURL, ytChannelId, index, controls, autoplay])
 
     const StyledIframe = styled("iframe")({
         width: "600px",
@@ -43,7 +45,8 @@ function YoutubEmbed({index = 0, controls = false, autoplay = false}:YoutubeEmbe
             loading="lazy"
             ref={ref}
             frameBorder={0}
-            allowFullScreen={true}/>
+            allowFullScreen={true}
+            src={url}/>
     )
 }
 
